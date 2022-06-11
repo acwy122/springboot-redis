7eb8ad20353e0c7f83207cdc397f948673f13ca6
xurb <xurongbiao@hmtim.com>
2022-06-11 23:59:55 +0800

    启动类调整

--- src/main/java/com/msb/ProductApplication.java
+++ src/main/java/com/msb/ProductApplication.java
@@ -1,12 +1,13 @@
 package com.msb;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.msb.mapper")
 public class ProductApplication {
     public static void main(String[] args) {
-        SpringBootApplication.run(ProductApplication.class,args);
+        SpringApplication.run(ProductApplication.class,args);
     }
 }
