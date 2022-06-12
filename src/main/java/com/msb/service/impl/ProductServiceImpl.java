5d1acf94680a3fb59d85d2331d38d813f106a1e3
xurb <xurongbiao@hmtim.com>
2022-06-12 16:45:25 +0800

    redis集群搭建，以及项目整合代码

--- src/main/java/com/msb/service/impl/ProductServiceImpl.java
+++ src/main/java/com/msb/service/impl/ProductServiceImpl.java
@@ -4,6 +4,8 @@ import com.msb.mapper.ProductMapper;
 import com.msb.pojo.Product;
 import com.msb.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +14,23 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductMapper productMapper;
 
+    @Autowired
+    private RedisTemplate<String ,Object> redisTemplate;
+
     @Override
     public Product findProductById(Integer id) {
-        return productMapper.findProductById(id);
+
+        String key = "Product:"+id;
+        //先从redis中获取数据
+        if(redisTemplate.hasKey(key)){
+            System.out.println("执行缓存");
+            redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Product>(Product.class));
+            Product product = (Product) redisTemplate.opsForValue().get(key);
+            return product;
+        }
+        System.out.println("执行mysql");
+        Product product = productMapper.findProductById(id);
+        redisTemplate.opsForValue().set(key,product);
+        return product;
     }
 }
